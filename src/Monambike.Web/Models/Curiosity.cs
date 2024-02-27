bb7aaf237ac9944f95326e965787543558490571
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2024-02-26 22:07:06 -0300

    Apply several changes

--- src/Monambike.Web/Models/Curiosity.cs
+++ src/Monambike.Web/Models/Curiosity.cs
@@ -1,24 +1,26 @@
-﻿namespace Monambike.Web.Data
+﻿namespace Monambike.Web.Models
 {
     public class Curiosity
     {
         public List<string> Curiosities { get; set; } = new List<string>();
 
+        /// <summary>
+        /// The curiosities file path.
+        /// </summary>
         public StreamReader CuriositiesFile = new(@"..\Resources\Curiosities.txt");
 
         public Curiosity()
         {
+            // Reads every line until the end of curiosities file
             while (!CuriositiesFile.EndOfStream)
             {
+                // Getting the line
                 var line = CuriositiesFile.ReadLine();
+                // If the line is not null, add into the list
                 if (line is not null) Curiosities.Add(line);
             }
         }
 
-        private string GetRandomCuriosity()
-        {
-            var random = new Random();
-            return Curiosities[random.Next(1, Curiosities.Count)];
-        }
+        private string GetRandomCuriosity() => Curiosities[new Random().Next(1, Curiosities.Count)];
     }
 }
