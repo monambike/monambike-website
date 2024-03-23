8828c1d0e463fc326bf26817727c9adbd5e3f0aa
Vinícius Gabriel <vinicius_gabriel258@hotmail.com>
2024-03-23 17:54:20 -0300

    Rename "Curiosity" to "RandomPhrase"

---
+++ src/Monambike.Web/Models/RandomPhrase.cs
@@ -0,0 +1,26 @@
+﻿namespace Monambike.Web.Models
+{
+    public class RandomPhrase
+    {
+        public List<string> Phrases { get; set; } = [];
+
+        /// <summary>
+        /// The phrases file path.
+        /// </summary>
+        public StreamReader PhrasesFile = new(@"..\Resources\Phrases.txt");
+
+        public RandomPhrase()
+        {
+            // Reads every line until the end of phrases file
+            while (!PhrasesFile.EndOfStream)
+            {
+                // Getting the line
+                var line = PhrasesFile.ReadLine();
+                // If the line is not null, add into the list
+                if (line is not null) Phrases.Add(line);
+            }
+        }
+
+        private string GetRandomPhrases() => Phrases[new Random().Next(1, Phrases.Count)];
+    }
+}
