e35c6355129570aa313b7260436a27c95bd7b9d6
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2026-09-20 23:04:52 -0300

    feat: set random phrases system

---
+++ src/Monambike.Web/Models/Phrase.cs
@@ -0,0 +1,18 @@
+﻿// Copyright(c) 2024 Vinicius Gabriel Marques de Melo. All rights reserved.
+// Contact: contact@monambike.com for more information.
+// For license information, please see the LICENSE file in the root directory.
+
+namespace Monambike.Web.Models
+{
+    public class Phrase
+    {
+        private readonly List<string> _phrases;
+
+        public Phrase(List<string> phrases)
+        {
+            _phrases = phrases;
+        }
+
+        public string GetRandomPhrase() => _phrases[new Random().Next(_phrases.Count)];
+    }
+}
